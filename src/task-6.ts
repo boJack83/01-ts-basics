5d27aa5dacf46c61676f7819fc057fa7655960c8
Serhii Kravcnenko <sergkrava1983@gmail.com>
2025-07-19 22:00:58 +0300

    fix-task-6

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,10 +2,6 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement([1, 2, 3]);           // 1
-getFirstElement(["a", "b", "c"]);     // "a"
-getFirstElement([true, false, true]); // true
-
-// console.log(getFirstElement([1, 2, 3]))
-// console.log(getFirstElement(["a", "b", "c"]))
-// console.log(getFirstElement([true, false, true]))
+getFirstElement<number>([1, 2, 3]);           // 1
+getFirstElement<string>(["a", "b", "c"]);     // "a"
+getFirstElement<boolean>([true, false, true]); // true
